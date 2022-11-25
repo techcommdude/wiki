2349f83e0c47fc13ce0a4e17afc17d0a1a6f2f26
GF <farnellg@gmail.com>
2022-11-25 12:01:08 -0500

    url working

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:entry>", views.entry, name="entry")
+    path("wiki/", views.index, name="index"),
+    path("wiki/<str:entry>", views.entry, name="entry")
 ]
