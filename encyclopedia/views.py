05d18a9473e581def67d3ad8695c811ab81f9b28
GF <farnellg@gmail.com>
2022-11-25 15:46:34 -0500

    loop works.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -12,10 +12,42 @@ def index(request):
 
 def displayEntry(request, entry):
 
+    # if util.get_entry(entry):
+    #     entryContent = entry
+    #     return render(request, "encyclopedia/entry.html", {
+    #         "entry": entry.capitalize()
+    #     })
+    # else:
+    #     return HttpResponse(f"Error, " + entry + " is not in the wiki!")
+
+    entries = []
+    entries = util.list_entries()
+    print(entries)
+
+    lower = []
+    lower = entries
+    for i in range(len(lower)):
+       lower[i] = lower[i].lower()
+    print(lower)
+
+    # for entry in entries:
+    #     print(entry)
+
+    # if entry.capitalize() in entries:
+    # print(entry)
+    # test = entries.index(entry)
+    # test2 = str(test)
+    # print(test2)
+
     if util.get_entry(entry):
-        entryContent = entry
+        # test = entries.index(entry)
+        entryContent = util.get_entry(entry)
+
+        # index = entries.index(entry)
+        # print(index)
+
         return render(request, "encyclopedia/entry.html", {
-            "entry": entry.capitalize()
+            "entry": entry
         })
     else:
         return HttpResponse(f"Error, " + entry + " is not in the wiki!")
