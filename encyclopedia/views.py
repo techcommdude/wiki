de3acb515cd6a1e0b57bf2d3a8be868b44fc9611
GF <farnellg@gmail.com>
2022-11-28 12:11:36 -0500

    change

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -13,36 +13,20 @@ def index(request):
 
 def displayEntry(request, displayEntry):
 
-    entries = []
-    entries = util.list_entries()
-    print(entries)
-
-    # matches = []
-    # for match in entries:
-    #     if displayEntry in match:
-    #         matches.append(match)
-
-    # print(matches)
-
-
+    # entries = []
+    # entries = util.list_entries()
+    # print(entries)
 
     #Use this to retrieve the entry to display.  Put it in a function?
     entryContents = util.get_entry(displayEntry)
 
-    # x = re.findall("[a-s]", entryContents, re.IGNORECASE)
-    # print(x)
-
-    # x = re.search(displayEntry, entryContents, re.IGNORECASE)
-    # print(x)
-
-    #if entryContents returns None, do not do this search since it leads to error.
 
     if entryContents != None:
 
         x = re.findall(displayEntry, entryContents, re.IGNORECASE)
 
         #If the list returned is not empty, print the first one.
-        print(x[0])
+        # print(x[0])
 
         return render(request, "encyclopedia/entry.html", {
             "displayEntry": x[0]
