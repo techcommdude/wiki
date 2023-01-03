29d2b6f157c008159d1d79bc80c54937ac14875e
GF <farnellg@gmail.com>
2023-01-03 10:23:53 -0500

    change

--- encyclopedia/models.py
+++ encyclopedia/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Topics(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     body = models.CharField(max_length=10000)
 
